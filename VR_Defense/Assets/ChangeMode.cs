a208744984ddf64de1575908c0638c887dabab73
JuanDeDiosGarcia <61006172+JuanDeDiosGarcia@users.noreply.github.com>
2023-05-11 12:18:00 +0200

    pATH

---
+++ VR_Defense/Assets/ChangeMode.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMode : MonoBehaviour
+{
+    public bool orda = false;
+    public bool build = true;
+  
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (Input.GetKeyDown(KeyCode.P))
+        //{
+        //    build = false;
+        //    orda = true;
+        //}
+    }
+}
---
+++ VR_Defense/Assets/Scripts/Enemies.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    private Transform target;
+
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        target = WayPoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(waypointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+
+        target = WayPoints.points[waypointIndex];
+    }
+
+
+
+
+
+}
