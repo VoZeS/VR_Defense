ba3bbea40bfac15164ffec2b1fd676482ca6d068
JuanDeDiosGarcia <juandediosgasal@gmail.com>
2023-06-01 19:35:50 +0200

    TowerUI

---
+++ VR_Defense/Assets/Scripts/UITowers.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UITowers : MonoBehaviour
+{
+
+
+
+    public TextMeshProUGUI arrowPriceUI;
+    public TextMeshProUGUI bombPriceUI;
+
+    public TextMeshProUGUI moneyUI;
+    public TextMeshProUGUI enemiesUI;
+
+
+    void Update()
+    {
+
+        enemiesUI.text = "Enemies Alive: " + WaveSpawner.enemyalive.ToString();
+        moneyUI.text = "Current Gold: " + PlayerStats.Money.ToString();
+
+        arrowPriceUI.text = "Price: " + Node.arrowTowerPrice.ToString();
+        bombPriceUI.text = "Price: " + Node.bombTowerPrice.ToString();
+
+
+
+    }
+}
